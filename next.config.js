425400341f24debd3b90635839562d57f5d3f34e
Mo Xu <edenx97@gmail.com>
2022-10-26 18:21:17 +1100

    Fix bugs

--- next.config.js
+++ next.config.js
@@ -35,11 +35,11 @@ const nextConfig = {
   pageExtensions: ["tsx", "mdx", "ts"],
   poweredByHeader: false,
   productionBrowserSourceMaps: false,
-  svgo: {
-    multipass: true,
-    plugins: ["removeDimensions"],
-  },
-  strictMode: true,
+  // svgo: {
+  //   multipass: true,
+  //   plugins: ["removeDimensions"],
+  // },
+  // strictMode: true,
   swcMinify: true,
   trailingSlash: false,
   images: {
